ee1e8711ba4d71599d82202a6407162ee49f613a
bccantwell <bcantwell@mills.edu>
2019-03-31 12:33:59 -0700

    Add files via upload

---
+++ Test_Classifier/test_classifer1.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 29 12:21:34 2019
+
+@author: admin
+"""
+
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+
+#Locate the CSV files
+filepath_dict = {'yelp':   '/Users/admin/Downloads/sentiment_analysis/yelp_labelled.txt',
+                 'amazon': '/Users/admin/Downloads/sentiment_analysis/amazon_cells_labelled.txt',
+                 'imdb':   '/Users/admin/Downloads/sentiment_analysis/imdb_labelled.txt'}
+
+
+#Print a test line from the CSV file 
+df_list = []
+for source, filepath in filepath_dict.items():
+    df = pd.read_csv(filepath, names=['sentence', 'label'], sep='\t')
+    df['source'] = source  
+    df_list.append(df)
+
+df = pd.concat(df_list)
+print(df.iloc[0])
+
+#Model 1: a baseline model using BOW and Logistic Regression
+
+for source in df['source'].unique():
+    df_source = df[df['source'] == source]
+    sentences = df_source['sentence'].values
+    y = df_source['label'].values
+
+    sentences_train, sentences_test, y_train, y_test = train_test_split(
+        sentences, y, test_size=0.25, random_state=1000)
+
+    #Implement the BOW model using CountVectorizer to tokenize the data
+    vectorizer = CountVectorizer()
+    vectorizer.fit(sentences_train)
+    X_train = vectorizer.transform(sentences_train)
+    X_test  = vectorizer.transform(sentences_test)
+
+    classifier = LogisticRegression()
+    classifier.fit(X_train, y_train)
+    score = classifier.score(X_test, y_test)
+    print('Accuracy for {} data: {:.4f}'.format(source, score))
+
+#Model 2: a keras deep learning model using the Sequential API
+
+from keras.models import Sequential
+from keras import layers
+
+#Find the dimensions of the feature space, give an overview of the model
+input_dim = X_train.shape[1]  # Number of features
+model = Sequential()
+model.add(layers.Dense(10, input_dim=input_dim, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.summary()
+
+
+from keras.preprocessing.text import Tokenizer
+
+#Use a tokenizer to pre-process the text for use by the embedding layer
+tokenizer = Tokenizer(num_words=5000)
+tokenizer.fit_on_texts(sentences_train)
+X_train = tokenizer.texts_to_sequences(sentences_train)
+X_test = tokenizer.texts_to_sequences(sentences_test)
+vocab_size = len(tokenizer.word_index) + 1
+
+from keras.preprocessing.sequence import pad_sequences
+
+#Pad the text sequences to make them all the same length (we're using length 100)
+maxlen = 100
+X_train = pad_sequences(X_train, padding='post', maxlen=maxlen)
+X_test = pad_sequences(X_test, padding='post', maxlen=maxlen)
+
+
+#Add an embedding layer
+embedding_dim = 50
+
+model = Sequential()
+model.add(layers.Embedding(input_dim=vocab_size, 
+                           output_dim=embedding_dim, 
+                           input_length=maxlen))
+model.add(layers.Flatten())
+model.add(layers.Dense(10, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+model.summary()
+
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
+
+#A function to plot testing and validation accuracy
+def plot_history(history):
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    x = range(1, len(acc) + 1)
+
+    plt.figure(figsize=(12, 5))
+    plt.subplot(1, 2, 1)
+    plt.plot(x, acc, 'b', label='Training acc')
+    plt.plot(x, val_acc, 'r', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.legend()
+    plt.subplot(1, 2, 2)
+    plt.plot(x, loss, 'b', label='Training loss')
+    plt.plot(x, val_loss, 'r', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+ 
+#Train and test the model, plot the accuracy    
+history = model.fit(X_train, y_train,
+                    epochs=20,
+                    verbose=False,
+                    validation_data=(X_test, y_test),
+                    batch_size=10)
+loss, accuracy = model.evaluate(X_train, y_train, verbose=False)
+print("Training Accuracy: {:.4f}".format(accuracy))
+loss, accuracy = model.evaluate(X_test, y_test, verbose=False)
+print("Testing Accuracy:  {:.4f}".format(accuracy))
+plot_history(history)
+
+
+
+
+
+
